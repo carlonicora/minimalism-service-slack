e7fbc32d59746ee353a306930c052256f939b19f
Carlo Nicora <carlo.nicora@gmail.com>
2020-12-26 17:23:54 +0100

    deprecated environments removed

--- src/Configurations/SlackConfigurations.php
+++ src/Configurations/SlackConfigurations.php
@@ -7,38 +7,14 @@ use Exception;
 class SlackConfigurations extends AbstractServiceConfigurations
 {
     /** @var string|null  */
-    private ?string $URL;
-
-    /** @var string|null  */
     private ?string $token;
-
-    /** @var string|null  */
-    private ?string $environment;
 
     /**
      * rabbitMqConfigurations constructor.
      * @throws Exception
      */
     public function __construct() {
-        $this->URL = getenv('MINIMALISM_SERVICE_SLACK_URL');
-        $this->environment = getenv('MINIMALISM_SERVICE_SLACK_ENVIRONMENT');
         $this->token = getenv('MINIMALISM_SERVICE_SLACK_TOKEN');
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getURL(): ?string
-    {
-        return $this->URL;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getEnvironment(): ?string
-    {
-        return $this->environment;
     }
 
     /**
